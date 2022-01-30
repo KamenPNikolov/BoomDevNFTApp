b96f9d9e76390885e11f6d2a52e289eb5a957883
knikolov <kami_985@abv.bg>
2022-01-30 04:09:24 +0200

    Header component

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -4,15 +4,18 @@ import Link from "../link/Link";
 import styles from "./Example.module.scss";
 import classNames from "classnames";
 import Logo from "../logo/Logo";
+import Header from "../header/Header";
 
 export default function Copyright() {
   return (
     <div className={classNames(styles.wrapper)}>
+      <Header></Header>
       <Container className={classNames(styles.container)} maxWidth="xl">
+        
         <Paper className={classNames(styles.paper)}>
           <Link href="/about" color="secondary">
             Hi
-          <Logo></Logo>
+          
           </Link>
         </Paper>
       </Container>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import classNames from "classnames";
+import Logo from '../logo/Logo';
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import styles from "./Header.module.scss"
+import Grid from "@mui/material/Grid";
+import Input from "@mui/material/Input";
+import SearchIcon from '@mui/icons-material/Search';
+import InputAdornment from '@mui/material/InputAdornment';
+
+
+export default function Header() {
+
+    return (
+    <div className={classNames(styles.wrapper)}>
+      <Container className={classNames(styles.container)}>
+      <Grid container spacing={10}>
+        <Grid item xs="auto">
+        <Logo></Logo>
+        </Grid>
+        <Grid item xs>
+        <Input className  = {classNames(styles.search)} defaultValue = "Find items, users and activities" startAdornment={<InputAdornment position="start"><SearchIcon className  = {classNames(styles.icon)}/></InputAdornment>}></Input>
+        </Grid>
+        <Grid item xs>
+        <Button className  = {classNames(styles.button)} variant="text">Home</Button>
+        <Button  variant="text">Activity</Button>
+        <Button   variant="contained" >Explore</Button>
+        </Grid>
+      </Grid>
+      </Container>
+    </div>      
+    );
+  }
