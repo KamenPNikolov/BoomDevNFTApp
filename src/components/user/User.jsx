ace5f419cb6abcc723a996aa26f0b9b001bd96cd
knikolov <kami_985@abv.bg>
2022-01-30 17:35:00 +0200

    Fixes for User assignment

--- src/components/user/User.jsx
+++ src/components/user/User.jsx
@@ -9,7 +9,7 @@ import Avatar from '../avatar/Avatar';
 export default function User({name = "", info = "",avatar = "",size = 55, verified = false}) {
 
     return (
-    <div className={classNames(styles.user)} style = {{width: size*3}} >  
+    <div className={classNames(styles.user)}  style = {{width: size*3}} >  
        <Avatar className={classNames(styles.avatar)} verified = {verified} size = {size} url = {avatar}></Avatar>
        
         <b className={classNames(styles.name)} > {name}</b>
