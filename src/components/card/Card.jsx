433e9e1ec38952b1bd6d2608db9165023846f709
knikolov <kami_985@abv.bg>
2022-01-30 22:35:40 +0200

    Card component implementation plus Example changes

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import classNames from "classnames";
+import Button from "@mui/material/Button";
+import styles from "./Card.module.scss";
+import TextField from "@mui/material/TextField";
+import Avatar from '../avatar/Avatar';
+import * as myModule from "@mui/material";
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import millify from "millify";
+import Chip from '@mui/material/Chip';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+
+export default function Card({name="",likes = 0, mediaUrl = "", user = {avatar: {url : ""},verified: false}, price = "", currency = ""}){
+
+    return (
+    
+    <myModule.Card sx={{ maxWidth: 500 }} className={classNames(styles.card)}>
+      <CardHeader
+        avatar={
+          <Avatar verified = {user.verified} url = {user.avatar.url}></Avatar>
+        }
+      />
+      <CardMedia className={classNames(styles.media)}
+        component="img"
+        image={mediaUrl}
+        alt="NFT Media"
+      />
+     <CardContent>
+       <div className={classNames(styles.title)}>{name}</div> 
+        <b className={classNames(styles.price)}>{price+" "+currency}</b>
+        <Chip color = "secondary" className={classNames(styles.likes)} variant="outlined" icon = {<FavoriteIcon></FavoriteIcon>} label = {millify(likes)}></Chip>
+      </CardContent>
+      <CardActions></CardActions>
+     </myModule.Card>
+    
+
+    );
+}
--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -7,11 +7,17 @@ import Logo from "../logo/Logo";
 import Header from "../header/Header";
 import Avatar from "../avatar/Avatar";
 import User from "../user/User";
+import Card from "../card/Card";
 
 export default function Copyright() {
+  const userProp = {avatar: {url: "/images/avatar.png"},verified: true};
   return (
     <div className={classNames(styles.wrapper)}>
-      <User verified = {true} size = {60} avatar = "/images/avatar.png" name = "kemimaro" info="140 items" ></User>  
+      <User verified = {true} size = {60} avatar = "/images/avatar.png" name = "kemimaro" info="140 items" ></User> 
+      <Card name = "Monkey" mediaUrl="https://cdn.vox-cdn.com/thumbor/2xj1ySLIz1EZ49NvSsPzq8Itjyg=/1400x1050/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/23084330/bored_ape_nft_accidental_.jpg"
+      user = {userProp} price = "12.4" currency = "ETH" likes={1100}
+      >
+        </Card> 
       <Container className={classNames(styles.container)} maxWidth="xl">
         
         <Paper className={classNames(styles.paper)}>
