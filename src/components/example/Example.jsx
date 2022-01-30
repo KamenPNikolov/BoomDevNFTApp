ea049e296dc0e72d2b2ff7dc55bb64b242d8945c
knikolov <kami_985@abv.bg>
2022-01-30 02:01:40 +0200

    Logo test fix - remove logo from example

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -12,7 +12,6 @@ export default function Copyright() {
         <Paper className={classNames(styles.paper)}>
           <Link href="/about" color="secondary">
             Hi
-            <Logo type = "muted"></Logo>
 
           </Link>
         </Paper>
--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -3,7 +3,6 @@ import classNames from "classnames";
 
 export default function Logo(props) {
 
-
   return (
     <div >
       <img src =  {props.type=="muted"?"/images/logo-muted.svg" :"/images/logo.svg"}></img>
