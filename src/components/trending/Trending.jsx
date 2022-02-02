641bf255998e4af74322ac2d9e2c5166024fddbd
knikolov <kami_985@abv.bg>
2022-02-02 09:52:03 +0200

    Trending changes to adjust to test case

--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -9,7 +9,7 @@ import MenuItem from '@mui/material/MenuItem';
 //always 4 cards displayed
 export default function Trending({cards = []}){
     const [c1,c2,c3,c4] = cards;
-    const name = c1.name;
+    
 
     return (
         <div className={classNames(styles.wrapper)}>
@@ -22,7 +22,7 @@ export default function Trending({cards = []}){
     </Select> 
     <Grid className={classNames(styles.grid)}  container spacing={3}>
         <Grid item lg>
-        <Card name={name} mediaUrl={c1.mediaUrl} user = {c1.user} price = {c1.price} currency =  {c1.currency} likes={c1.likes} ></Card>
+        <Card name={c1.name} mediaUrl={c1.mediaUrl} user = {c1.user} price = {c1.price} currency =  {c1.currency} likes={c1.likes} ></Card>
         </Grid>
         <Grid item lg>
         <Card name={c2.name} mediaUrl={c2.mediaUrl} user = {c2.user} price = {c2.price} currency =  {c2.currency} likes={c2.likes} ></Card>
