c42d9608a5125eddf5bc5d1a3aded1053e4acbd5
knikolov <kami_985@abv.bg>
2022-02-08 01:27:35 +0200

    Collector: Finishing styles, adding grid and number

--- src/components/collectors/Collector.jsx
+++ src/components/collectors/Collector.jsx
@@ -7,11 +7,21 @@ import Container from "@mui/material/Container";
 import User from "../user/User";
 
 
-export default function Collector({name = "",avatar = "", verified = false,type = 0, nftCount = 0}) {
+
+
+export default function Collector({name = "",avatar = "", verified = false,type = 0, nftCount = 0,id = 0}) {
     const darkOrLight = (type==0?styles.dark:styles.light);
+    const darkOrLightNumber = (type==0?styles.darknumber:styles.lightnumber);
   return (
     <div className={classNames(styles.container,darkOrLight )} >
-        <User   verified = {verified}  avatar = {avatar} name = {name} info={nftCount + " items"} ></User>
+        <Grid container  direction="row" justifyContent="space-between"  alignItems="stretch">
+        <Grid item xs="3" className={classNames(darkOrLightNumber )} >
+            <b className={classNames(styles.number)} >{id}</b>
+        </Grid>
+        <Grid  item xs = "8" >
+        <User  className={classNames(styles.user)} verified = {verified}  avatar = {avatar} name = {name} info={nftCount + " items"} ></User>
+        </Grid>
+        </Grid>    
     </div>
     
   );
